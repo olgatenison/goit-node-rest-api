8fe85d2f65a7438fb8f9d7aa2fffcc3c0d7f542a
olgatenison <88651744+olgatenison@users.noreply.github.com>
2024-02-25 17:57:43 +0200

    update favorite

--- db/contact.js
+++ db/contact.js
@@ -1,20 +1,28 @@
 import { Schema, model } from "mongoose";
 
-const contactSchema = new Schema({
-  name: {
-    type: String,
-    required: [true, "Set name for contact"],
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Set name for contact"],
+    },
+    email: {
+      type: String,
+    },
+    phone: {
+      type: String,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
   },
-  email: {
-    type: String,
-  },
-  phone: {
-    type: String,
-  },
-  favorite: {
-    type: Boolean,
-    default: false,
-  },
+  { versionKey: false, timestamps: true }
+);
+
+contactSchema.post("save", (error, data, next) => {
+  error.status = 400;
+  next();
 });
 
 const Contact = model("contact", contactSchema);
--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -4,10 +4,16 @@ export const createContactSchema = Joi.object({
   name: Joi.string().optional(),
   email: Joi.string().email().optional(),
   phone: Joi.string().optional(),
+  favorite: Joi.boolean().optional(),
 });
 
 export const updateContactSchema = Joi.object({
   name: Joi.string(),
   email: Joi.string(),
   phone: Joi.number(),
+  favorite: Joi.boolean().optional(),
+});
+
+export const updateFavoriteSchema = Joi.object({
+  favorite: Joi.boolean().required(),
 });
