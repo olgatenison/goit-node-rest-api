9a262ec41a997184d09d2221f0ff3d0a3af71bc3
olgatenison <88651744+olgatenison@users.noreply.github.com>
2024-03-15 00:39:08 +0200

    5 -01

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ mongoose
 app.use(morgan("tiny"));
 app.use(cors());
 app.use(express.json());
+app.use(express.static("public"));
 
 app.use("/api/auth", authRouter);
 app.use("/api/contacts", contactsRouter);
---
+++ helpers/upload.js
@@ -0,0 +1,22 @@
+import multer from "multer";
+import path from "path";
+
+// import { fileURLToPath } from "url";
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+
+const tempDir = path.join(__dirname, "../", "temp");
+
+const multerConfig = multer.diskStorage({
+  destination: tempDir,
+  filename: (req, file, cd) => {
+    cd(null, file.originalname);
+  },
+});
+
+const upload = multer({
+  storage: multerConfig,
+});
+
+export default upload;
